e87da16b4c3cefa01cc1bf0949dff35598a002c6
Hind Github <hindgithub@gmail.com>
2015-03-18 14:43:05 +0100

    Soap service to get all books.

--- src/main/java/com/hind/elibrary/webservice/soap/WsSoapController.java
+++ src/main/java/com/hind/elibrary/webservice/soap/WsSoapController.java
@@ -1,20 +1,38 @@
 package com.hind.elibrary.webservice.soap;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.hind.elibrary.model.Book;
 import com.hind.elibrary.service.BookService;
+import com.hind.elibrary.webservice.soap.model.BookResponse;
+import com.hind.elibrary.webservice.soap.model.BookType;
 
 @Endpoint
 public class WsSoapController {
 
-	private static final String NAMESPACE_URI = "http://hind.com/elibrary";
+	private static final String NAMESPACE_URI = "http://hind.com/elibrary/webservice/soap/model";
 
 	private BookService bookService;
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "BookRequest")
-	public void handleBookRequest() {
-		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	@ResponsePayload
+	public BookResponse handleBookRequest() {
+		Collection<Book> books = bookService.getAllBooks();
+		Collection<BookType> resultBooks = new LinkedList<BookType>();
+		for (Book book : books) {
+			BookType bookType = new BookType();
+			bookType.setAuthor(book.getAuthor());
+			bookType.setTitle(book.getTitle());
+			resultBooks.add(bookType);
+		}
+		BookResponse response = new BookResponse();
+		response.getBook().addAll(resultBooks);
+		return response;
 	}
 
 	public BookService getBookService() {
