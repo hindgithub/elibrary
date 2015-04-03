fe31f267ee423aebe8c3c34a7f6e3e6ed8c84eb4
Hind Github <hindgithub@gmail.com>
2015-04-03 14:51:53 +0200

    Test configuratio for dao.

--- src/test/java/com/hind/elibrary/dao/BookDaoImplTest.java
+++ src/test/java/com/hind/elibrary/dao/BookDaoImplTest.java
@@ -1,5 +1,24 @@
 package com.hind.elibrary.dao;
 
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/com/hind/elibrary/test/dao/spring-context-configuration-test-dao.xml")
 public class BookDaoImplTest {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void test() {
+		Assert.assertNull(dataSource);
+	}
 
 }
